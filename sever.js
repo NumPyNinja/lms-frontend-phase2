1cb7be736b4b49672cf691d0479ba41828fb0a81
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 21:19:23 -0400

    initial commit

--- sever.js
+++ sever.js
@@ -1,20 +1,27 @@
-//Install express server
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/lms-frontend-phase2'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/lms-frontend-phase2/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+
+
+// Install express server 
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lms-frontend-phase2'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/lms-frontend-phase2/'}),
-);
-
+app.use(express.static(__dirname + '/dist/lms-frontend-phase2'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/lms-frontend-phase2/index.html'));
+});
 // Start the app by listening on the default Heroku port
-
-
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 8080);
