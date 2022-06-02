ac10c30d29e1f83b7d965dfef8c2d709ff32b88a
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 21:02:01 -0400

    initial commit

--- sever.js
+++ sever.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/LMS-FrontEnd'));
+app.use(express.static('./dist/lms-frontend-phase2'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/LMS-FrontEnd/'}),
+    res.sendFile('index.html', {root: 'dist/lms-frontend-phase2/'}),
 );
 
 // Start the app by listening on the default Heroku port
