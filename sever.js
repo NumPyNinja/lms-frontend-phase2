963a5dc2584fcccd4ee20da6a498382c19770911
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 23:01:16 -0400

    merged lms-frontend changes

--- sever.js
+++ sever.js
@@ -4,14 +4,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// var cors = require('cors');
-// app.use(cors());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+const HOST = "localhost";
+const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+
+// Proxy endpoints
+app.use('/api', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/api`]: '',
+    },
+ }));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/lms-frontend-phase2'));
--- src/app/program/program.service.ts
+++ src/app/program/program.service.ts
@@ -14,7 +14,7 @@ export class ProgramService {
 
   getPrograms(): Observable<Program[]> {
     // return this.httpClient.get<Program[]>('assets/Programs.json')
-    return this.httpClient.get<Program[]>("https://lms-phase2.herokuapp.com/lms/allPrograms");
+    return this.httpClient.get<Program[]>(this.url + "/allPrograms");
   }
 
   addProgram(program: Program): Observable<Program> {
