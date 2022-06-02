a33ea1913af33d67552f77d1254067b2444e1888
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 22:34:04 -0400

    merged lms-frontend changes

--- src/app/program/program.service.ts
+++ src/app/program/program.service.ts
@@ -14,7 +14,7 @@ export class ProgramService {
 
   getPrograms(): Observable<Program[]> {
     // return this.httpClient.get<Program[]>('assets/Programs.json')
-    return this.httpClient.get<Program[]>(this.url + "/allPrograms");
+    return this.httpClient.get<Program[]>("https://lms-admin-rest-service.herokuapp.com/allPrograms");
   }
 
   addProgram(program: Program): Observable<Program> {
