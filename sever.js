4045a1ae01a8fca2a34885c8116f214184046b39
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 22:25:54 -0400

    merged lms-frontend changes

--- sever.js
+++ sever.js
@@ -4,14 +4,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-var cors = require('cors');
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// var cors = require('cors');
+// app.use(cors());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/lms-frontend-phase2'));
