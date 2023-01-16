929f6ab3ddd1c6bfeaa985d0b891d5489455ecd3
Priya <priya@Priyas-MacBook-Pro.local>
2023-01-16 17:44:05 -0500

    forgot password

--- src/app/login/forgot-password/forgot-password.component.ts
+++ src/app/login/forgot-password/forgot-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  [x: string]: any;
+  hide = true;
+  textInput = 'Link has sent on your registered email !!!';
+  firstFormGroup: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
   ngOnInit(): void {
+
+    this.firstFormGroup = this.fb.group({
+
+      email: ['', [Validators.required, Validators.email]]
+    })
+
   }
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? '' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  onClick() {
+
+    this.displayValue = this.textInput;
+  }
+  get email() { return this.firstFormGroup.get('email'); }
+
 
 }
