4a2f68db1a72822762672e1cc7d803948666e8b8
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2023-04-03 17:28:05 -0400

    Update sign-up.service.ts

--- src/app/login/sign-up/sign-up.service.ts
+++ src/app/login/sign-up/sign-up.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SignUp } from './sign-up';
 import { Observable } from 'rxjs';
-
+import { environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
