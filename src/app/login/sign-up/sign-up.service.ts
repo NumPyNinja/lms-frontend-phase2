39e00da8b20131ffac3628719872ce1e462cdaf1
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2023-04-03 17:30:43 -0400

    Update sign-up.service.ts

--- src/app/login/sign-up/sign-up.service.ts
+++ src/app/login/sign-up/sign-up.service.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SignUp } from './sign-up';
 import { Observable } from 'rxjs';
-import { environment} from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpService {
 
-  baseUrl = environment.baseUrl;
+  
 
-  //url:string = "api";//'https://lms-admin-rest-service.heroku.com/signup';
+  url:string = "api";//'https://lms-admin-rest-service.heroku.com/signup';
 
   constructor(private httpClient: HttpClient) { }
 
