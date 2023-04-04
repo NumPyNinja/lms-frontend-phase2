81683bbec78fabd01b924e8da88841531a66aa14
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2023-04-03 22:35:53 -0400

    Update sever.js

--- sever.js
+++ sever.js
@@ -9,9 +9,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 const HOST = "localhost";
-const API_SERVICE_URL = "https://lms-phase2.herokuapp.com/lms/";
+//const API_SERVICE_URL = "https://lms-phase2.herokuapp.com/lms/";
 
-//const API_SERVICE_URL = "https://lms-backend-service.herokuapp.com/lms/";
+const API_SERVICE_URL = "https://lms-backend-service.herokuapp.com/lms/";
 // Set allowed methods header
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 // Proxy endpoints
 app.use('/api', createProxyMiddleware({
     target: API_SERVICE_URL,
-    changeOrigin: true,
+    changeOrigin: false,
     pathRewrite: {
         [`^/api`]: '',
     },
