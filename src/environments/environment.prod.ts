0bfa072a8e29dcafd1270e08599f7a68c575af12
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2023-04-03 23:24:44 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:'https://lms-backend-service.herokuapp.com/lms/'  //heroku
+  baseUrl:'https://lms-backend-service.herokuapp.com/lms'  //heroku
 
   
 };
